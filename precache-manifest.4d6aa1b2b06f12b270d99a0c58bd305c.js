1b4d1a0e95160b2abc95ac0553ec0a69bb89040d
xxluciferinxx <mukulbisht98@gmail.com>
2019-07-17 00:48:37 +0530

    Updates

---
+++ precache-manifest.4d6aa1b2b06f12b270d99a0c58bd305c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1dc439cc546526975a47cb31750e6c6",
+    "url": "/book-my-hall/index.html"
+  },
+  {
+    "revision": "287365560960bf585115",
+    "url": "/book-my-hall/static/css/main.855eb62d.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/book-my-hall/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "287365560960bf585115",
+    "url": "/book-my-hall/static/js/main.094d25d7.chunk.js"
+  },
+  {
+    "revision": "edc65f3fb48e40cc5c73",
+    "url": "/book-my-hall/static/js/runtime~main.a33118c4.js"
+  }
+]);
